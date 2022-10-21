6e247ea3cb4db1fa35a83e76298d0f28bc37425e
hengxin <hfwei@nju.edu.cn>
2022-10-21 21:05:44 +0800

    +4-loops

--- 4-loops/bubble-sort.c
+++ 4-loops/bubble-sort.c
@@ -1,6 +1,9 @@
 //
 // Created by hengxin on 10/19/22.
 //
+// Please first read the Bubble Sort wiki page: https://en.wikipedia.org/wiki/Bubble_sort,
+// and think about it before reading the code.
+//
 // For the usage of clock(),
 // please refer to https://legacy.cplusplus.com/reference/ctime/clock/.
 //
@@ -10,14 +13,40 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define LEN 100000
+#define LEN 1000000
 int numbers[LEN] = {0};
 
 int main() {
+  srand(time(NULL));
+  for (int i = 0; i < LEN; i++) {
+    numbers[i] = rand() % LEN;
+  }
+
+  // set a clock
+  clock_t start = clock();
+
+  bool swapped = true;
+  for (int i = 0; i < LEN && swapped; i++) {
+    swapped = false;
+    for (int j = 0; j < LEN - 1 - i; j++) {
+      if (numbers[j] > numbers[j + 1]) {
+        int temp = numbers[j];
+        numbers[j] = numbers[j + 1];
+        numbers[j + 1] = temp;
+        swapped = true;
+      }
+    }
+  }
+
+  // record the end clock
+  clock_t end = clock();
 
   for (int i = 0; i < LEN; i++) {
     printf("%d ", numbers[i]);
   }
+
+  long sec = (end - start) / CLOCKS_PER_SEC;
+  printf("Sorting %d numbers in %ld seconds.\n", LEN, sec);
 
   return 0;
 }
--- 4-loops/game-of-life.c
+++ 4-loops/game-of-life.c
@@ -7,8 +7,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define MAX_ROUND 10
 #define SIZE 6
-const int board[SIZE][SIZE] = {
+int board[SIZE][SIZE] = {
     {0},
     {0, 1, 1, 0, 0, 0},
     {0, 1, 1, 0, 0, 0},
@@ -17,7 +18,7 @@ const int board[SIZE][SIZE] = {
     {0}
 };
 
-//const int board[SIZE][SIZE] = {
+//int board[SIZE][SIZE] = {
 //    [1][1] = 1, [1][2] = 1,
 //    [2][1] = 1, [2][2] = 1,
 //    [3][3] = 1, [3][4] = 1,
@@ -26,8 +27,71 @@ const int board[SIZE][SIZE] = {
 
 int main() {
   // add code here
+  // extend to board[SIZE + 2][SIZE + 2]
+  int old_board[SIZE + 2][SIZE + 2];
+  for(int row = 0; row < SIZE + 2; row++) {
+    for(int col =0; col < SIZE + 2; col++) {
+      if (row == 0 || row == SIZE + 1 || col == 0 || col == SIZE + 1) {
+        old_board[row][col] = 0;
+      } else {
+        old_board[row][col] = board[row - 1][col - 1];
+      }
+    }
+  }
 
   // print the original board
+  system("clear");
+  for(int row = 1; row <= SIZE; row++) {
+    for(int col = 1; col <= SIZE; col++) {
+      printf("%c ", old_board[row][col] ? '*' : ' ');
+    }
+    printf("\n");
+  }
+
+  int next_board[SIZE + 2][SIZE + 2] =  {0};
+
+  for(int round = 1; round <= MAX_ROUND; round++) {
+    for (int row = 1; row <= SIZE; row++) {
+      for (int col = 1; col <= SIZE; col++) {
+        // counting the number of neighbours
+        int neighbours =
+            old_board[row - 1][col - 1] +
+                old_board[row - 1][col] +
+                old_board[row - 1][col + 1] +
+                old_board[row][col - 1] +
+                old_board[row][col + 1] +
+                old_board[row + 1][col - 1] +
+                old_board[row + 1][col] +
+                old_board[row + 1][col + 1];
+
+        // evaluate the next_board[row][col] in the next_board
+        if (old_board[row][col]) {
+          next_board[row][col] = (neighbours == 2 || neighbours == 3);
+        } else {
+          next_board[row][col] = (neighbours == 3);
+        }
+      }
+    }
+
+    // print the next_board
+    // wins: #include <windows.h>: Sleep(ms)
+    sleep(1);
+
+    // wins: #include <conio.h>: clrscr()
+    system("clear");
+    for (int row = 1; row <= SIZE; row++) {
+      for (int col = 1; col <= SIZE; col++) {
+        printf("%c ", next_board[row][col] ? '*' : ' ');
+      }
+      printf("\n");
+    }
+
+    for (int row = 0; row < SIZE + 2; row++) {
+      for (int col = 0; col < SIZE + 2; col++) {
+        old_board[row][col] = next_board[row][col];
+      }
+    }
+  }
 
 
   return 0;
