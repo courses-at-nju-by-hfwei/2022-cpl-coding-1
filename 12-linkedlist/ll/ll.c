e25c82584d12c47faddf1905ca82f1a8a656ef9c
Hengfeng Wei <hengxin>
2022-12-17 20:35:59 +0800

    ll.c: +comment

--- 12-linkedlist/ll/ll.c
+++ 12-linkedlist/ll/ll.c
@@ -61,7 +61,7 @@ void Delete(LinkedList *list, Node *prev) {
   if (IsEmpty(list) || IsSingleton(list)) {
     list->head = NULL;
     list->tail = NULL;
-    return;
+    return; // Warning: Forget to "free" the node when IsSingleton(list) is true
   }
 
   Node *cur = prev->next;
